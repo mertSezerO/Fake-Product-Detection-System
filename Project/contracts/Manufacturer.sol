b8280df907d311e47644a8ab65c19e362bcb6c67
mertSezerO <mertS.Oktay@gmail.com>
2024-02-16 22:05:05 +0300

    Manufacturer contract added and related images added.

---
+++ Project/contracts/Manufacturer.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Manufacturer {
+    address public owner;
+    ProductAction productAction;
+    SupplyChain supplyChain;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        productAction = new ProductAction();
+        supplyChain = new SupplyChain(productAction);
+    }
+
+    function addProduct() internal onlyOwner {
+        productAction.registerProduct();
+    }
+
+    function editProduct() internal onlyOwner {
+        productAction.updateProduct();
+    }
+
+    function addSupplier(address supplier) internal onlyOwner {
+        supplyChain.addSupplier(supplier);
+    }
+
+    function createTransaction(address supplier) internal onlyOwner {
+        supplyChain.createTransaction(supplier);
+    }
+}
