685dcdf1f901b887854cd6d0f13eadc1448527b2
mertSezerO <mertS.Oktay@gmail.com>
2024-02-26 21:24:33 +0300

    Compiling contracts done.

--- Project/backend/web3-controller/compile.js
+++ Project/backend/web3-controller/compile.js
@@ -2,7 +2,48 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const controllerPath = path.resolve(__dirname,"..\\..\\..\\", "contracts", "*.sol");
-const source = fs.readFileSync(controllerPath, "utf8");
+const controllerPath = path.resolve(__dirname,"..\\..\\", "contracts", "Controller.sol");
+const productPath = path.resolve(__dirname,"..\\..\\", "contracts", "ProductAction.sol");
+const supplyChainPath = path.resolve(__dirname,"..\\..\\", "contracts", "SupplyChain.sol");
+const typesPath = path.resolve(__dirname,"..\\..\\", "contracts", "Types.sol");
 
-module.exports = solc.compile(source, 1).contracts[":Controller"];
+const csource = fs.readFileSync(controllerPath, "utf8");
+const psource = fs.readFileSync(productPath, "utf8");
+const ssource = fs.readFileSync(supplyChainPath, "utf8");
+const tsource = fs.readFileSync(typesPath, "utf8");
+
+var input = {
+    language: 'Solidity',
+    sources: {
+      'Controller.sol': {
+        content: csource
+      },
+      'ProductAction.sol':{
+        content: psource
+      },
+      'SupplyChain.sol':{
+        content: ssource
+      },
+      'Types.sol':{
+        content: tsource
+      }
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['*']
+        }
+      }
+    }
+  };
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)))
+// module.exports = output.contracts["Controller"].Controller;
+module.exports = {
+    ControllerABI: output.contracts['Controller.sol']['Controller'].abi,
+    ControllerBytecode: output.contracts['Controller.sol']['Controller'].evm.bytecode.object,
+    ProductActionABI: output.contracts['ProductAction.sol']['ProductAction'].abi,
+    ProductActionBytecode: output.contracts['ProductAction.sol']['ProductAction'].evm.bytecode.object,
+    SupplyChainABI: output.contracts['SupplyChain.sol']['SupplyChain'].abi,
+    SupplyChainBytecode: output.contracts['SupplyChain.sol']['SupplyChain'].evm.bytecode.object,
+};
