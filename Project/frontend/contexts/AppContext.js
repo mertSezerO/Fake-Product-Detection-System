b2f323aa050b3c98d2ff683fc40565f6fa641d0b
mertSezerO <mertS.Oktay@gmail.com>
2024-04-26 21:28:28 +0300

    Context state addition to dynamically acquire network address.

--- Project/frontend/contexts/AppContext.js
+++ Project/frontend/contexts/AppContext.js
@@ -3,17 +3,20 @@ import { createContext, useState } from "react";
 const AppContext = createContext();
 
 export default function AppProvider({ children }) {
-    const [userToken, setUserToken] = useState();
-    const [userRole, setUserRole] = useState();
-      
-    const state = {
-        userToken,
-        setUserToken,
-        userRole,
-        setUserRole
-    };
-    
-    return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
+  const [userToken, setUserToken] = useState();
+  const [userRole, setUserRole] = useState();
+  const [networkIP, setNetworkIP] = useState();
+
+  const state = {
+    userToken,
+    setUserToken,
+    userRole,
+    setUserRole,
+    networkIP,
+    setNetworkIP,
+  };
+
+  return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
 }
 
 export { AppContext };
