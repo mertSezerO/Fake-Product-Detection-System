698fdc9f315b84db27adeeb4bc9f35d1dba8d3a0
gamzensahin <gamzenadiyesahin@hotmail.com>
2024-05-04 14:56:49 +0300

    Address matching bug fixation.

--- Project/backend/web3-controller/controllers/supply-chain.js
+++ Project/backend/web3-controller/controllers/supply-chain.js
@@ -1,75 +1,77 @@
-const { contracts, logger } = require("../utils")
-const { SupplyChain } = contracts
+const axios = require("axios");
+const { contracts, logger } = require("../utils");
+const { SupplyChain } = contracts;
 
 exports.getProductTransactions = async (req, res, next) => {
-  const { productId } = req.params
+  const { productId } = req.params;
 
   try {
-    const address = await SupplyChain.methods.owner().call()
+    const address = await SupplyChain.methods.owner().call();
 
     SupplyChain.methods
       .getProductHistory(productId)
-      .send({ from: address, gas: 1000000 })
+      .send({ from: address, gas: 1000000 });
 
     const supplyEvent = await new Promise((resolve) => {
       SupplyChain.events
         .ProductTransactionAccessed()
-        .once("data", (event) => resolve(event))
-    })
+        .once("data", (event) => resolve(event));
+    });
 
     if (supplyEvent.returnValues.productId !== productId) {
       throw ((message, status) => {
-        const error = new Error(message)
-        error.status = status
-        return error
-      })("There is an internal error", 500)
+        const error = new Error(message);
+        error.status = status;
+        return error;
+      })("There is an internal error", 500);
     }
-    const { transactions } = supplyEvent.returnValues
+    const { transactions } = supplyEvent.returnValues;
 
     logger.info(
       `Transactions of Product with ID: ${productId}, transactions:${transactions}`
-    )
+    );
 
     return res.status(200).json({
       message: "Transactions of product successfully retrieved.",
       productId: productId,
       transactions: transactions,
-    })
+    });
   } catch (error) {
     if (error.status) {
       return res.status(error.status).json({
         message: error.message,
-      })
+      });
     } else {
       return res.status(503).json({
         message: error.message,
-      })
+      });
     }
   }
-}
+};
 
 exports.createTransaction = async (req, res, next) => {
-  const { productId } = req.params
-  const { companyName, condition } = req.body
+  const { productId } = req.params;
+  const { companyName, condition } = req.body;
 
   try {
-    const address = await SupplyChain.methods.owner().call()
+    const address = await SupplyChain.methods.owner().call();
 
-    const response = await axios.post("http://192.168.68.51:3000/match", {
+    console.log(companyName);
+    const response = await axios.post("http://192.168.68.51:3001/match", {
       companyName: companyName,
-    })
+    });
 
-    const receiver = response.data.address
+    const receiver = response.data.address;
 
     SupplyChain.methods
       .recordTransaction(productId, response.data.address, condition)
-      .send({ from: address, gas: 1000000 })
+      .send({ from: address, gas: 1000000 });
 
     const supplyEvent = await new Promise((resolve) => {
       SupplyChain.events
         .TransactionCreated()
-        .once("data", (event) => resolve(event))
-    })
+        .once("data", (event) => resolve(event));
+    });
 
     if (
       supplyEvent.returnValues.receiver !== receiver ||
@@ -77,18 +79,18 @@ exports.createTransaction = async (req, res, next) => {
       supplyEvent.returnValues.condition !== condition
     ) {
       throw ((message, status) => {
-        const error = new Error(message)
-        error.status = status
-        return error
-      })("There is an internal error", 500)
+        const error = new Error(message);
+        error.status = status;
+        return error;
+      })("There is an internal error", 500);
     }
-    const { sender } = supplyEvent.returnValues
+    const { sender } = supplyEvent.returnValues;
 
     logger.info(
       `New transaction for product with ID: ${productId}, transaction: ${JSON.stringify(
         { sender: sender, receiver: receiver, condition: condition }
       )}`
-    )
+    );
 
     return res.status(201).json({
       message: "Transaction created.",
@@ -98,56 +100,58 @@ exports.createTransaction = async (req, res, next) => {
         receiver: receiver,
         condition: condition,
       },
-    })
+    });
   } catch (error) {
     if (error.status) {
       return res.status(error.status).json({
         message: error.message,
-      })
+      });
     } else {
       return res.status(503).json({
         message: error.message,
-      })
+      });
     }
   }
-}
+};
 
 exports.addNewSupplier = async (req, res, next) => {
-  const { supplier } = req.body
+  const { supplier } = req.body;
 
   try {
-    const address = await SupplyChain.methods.owner().call()
+    const address = await SupplyChain.methods.owner().call();
     SupplyChain.methods
       .addSupplier(supplier)
-      .send({ from: address, gas: 1000000 })
+      .send({ from: address, gas: 1000000 });
 
     const supplyEvent = await new Promise((resolve) => {
-      SupplyChain.events.SupplierAdded().once("data", (event) => resolve(event))
-    })
+      SupplyChain.events
+        .SupplierAdded()
+        .once("data", (event) => resolve(event));
+    });
 
     if (supplyEvent.returnValues.supplier !== supplier) {
       throw ((message, status) => {
-        const error = new Error(message)
-        error.status = status
-        return error
-      })("There is an internal error", 500)
+        const error = new Error(message);
+        error.status = status;
+        return error;
+      })("There is an internal error", 500);
     }
 
-    logger.info(`New supplier added with address: ${supplier}`)
+    logger.info(`New supplier added with address: ${supplier}`);
 
     return res.status(200).json({
       message: "Supplier successfully added.",
       supplier: supplier,
-    })
+    });
   } catch (error) {
     if (error.status) {
       return res.status(error.status).json({
         message: error.message,
-      })
+      });
     } else {
       return res.status(503).json({
         message: error.message,
-      })
+      });
     }
   }
-}
+};
--- Project/frontend/screens/CreateTransactionScreen.js
+++ Project/frontend/screens/CreateTransactionScreen.js
@@ -6,26 +6,26 @@ import {
   TextInput,
   Button,
   Modal,
-} from "react-native"
-import React from "react"
-import tailwindConfig from "../tailwind.config"
-import { StatusBar } from "expo-status-bar"
-import { useState, useContext } from "react"
-import { ArrowLeftIcon } from "react-native-heroicons/solid"
-import { useNavigation } from "@react-navigation/native"
+} from "react-native";
+import React from "react";
+import tailwindConfig from "../tailwind.config";
+import { StatusBar } from "expo-status-bar";
+import { useState, useContext } from "react";
+import { ArrowLeftIcon } from "react-native-heroicons/solid";
+import { useNavigation } from "@react-navigation/native";
 
-import { CoreContext } from "../contexts/CoreContext"
+import { CoreContext } from "../contexts/CoreContext";
 
 const CreateTransactionScreen = () => {
-  const [receiver, setReceiver] = useState("")
-  const [productCondition, setProductCondition] = useState("")
-  const [modalVisible, setModalVisible] = useState(false)
+  const [companyName, setCompanyName] = useState("");
+  const [productCondition, setProductCondition] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
 
-  const navigation = useNavigation()
-  const coreContext = useContext(CoreContext)
+  const navigation = useNavigation();
+  const coreContext = useContext(CoreContext);
 
   const handleTransaction = async () => {
-    const { productId } = coreContext.selectedProduct
+    const { productId } = coreContext.selectedProduct;
     try {
       const response = await fetch(
         "http://192.168.68.51:3000/supply-chain/" + productId,
@@ -35,37 +35,40 @@ const CreateTransactionScreen = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            receiver: receiver,
+            companyName: companyName,
             condition: productCondition,
           }),
         }
-      )
+      );
 
       if (response.status !== 201) {
-        throw new Error("HTTP error, status = " + response.status)
+        throw new Error("HTTP error, status = " + response.status);
       }
 
-      setModalVisible(true)
+      setModalVisible(true);
     } catch (error) {
-      console.error("Create Transaction error:", error.message)
+      console.error("Create Transaction error:", error.message);
     }
-  }
+  };
 
   return (
     <ImageBackground
       className="flex-1"
-      source={require("../assets/images/bg.png")}>
+      source={require("../assets/images/bg.png")}
+    >
       <View className="flex-1 justify-start">
         <TouchableOpacity
           className="p-3 ml-4 mt-8"
-          onPress={() => navigation.navigate("Admin")}>
+          onPress={() => navigation.navigate("Admin")}
+        >
           <ArrowLeftIcon size="30" color="white" />
         </TouchableOpacity>
       </View>
       <View className="flex-1 flex justify-end">
         <View
           className="flex-2/3 bg-white px-10 pt-10"
-          style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}>
+          style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
+        >
           <Text className="text-gray font-bold text-4xl text-center">
             {coreContext.selectedProduct.productName}
           </Text>
@@ -77,9 +80,10 @@ const CreateTransactionScreen = () => {
               </Text>
               <TextInput
                 className="p-4 bg-gray-200 text-gray-700 rounded-2xl mb-5"
-                placeholder="Enter Destination Address"
-                value={receiver}
-                onChangeText={(text) => setReceiver(text)}></TextInput>
+                placeholder="Enter Company Name"
+                value={companyName}
+                onChangeText={(text) => setCompanyName(text)}
+              ></TextInput>
               <Text className="text-gray-700 font-bold mb-1 ml-4">
                 Condition
               </Text>
@@ -87,12 +91,14 @@ const CreateTransactionScreen = () => {
                 className="p-4 bg-gray-200 text-gray-700 rounded-2xl"
                 placeholder="Enter Product Condition"
                 value={productCondition}
-                onChangeText={(text) => setProductCondition(text)}></TextInput>
+                onChangeText={(text) => setProductCondition(text)}
+              ></TextInput>
             </View>
             <View className="mb-20 mt-20 space-y-4">
               <TouchableOpacity
                 className="py-4 bg-gray-900 rounded-xl"
-                onPress={handleTransaction}>
+                onPress={handleTransaction}
+              >
                 <Text className="font-3xl text-white font-extrabold text-center">
                   Create Transaction
                 </Text>
@@ -101,26 +107,30 @@ const CreateTransactionScreen = () => {
                 animationType="slide"
                 transparent={true}
                 visible={modalVisible}
-                onRequestClose={() => setModalVisible(false)}>
+                onRequestClose={() => setModalVisible(false)}
+              >
                 <View
                   style={{
                     flex: 1,
                     justifyContent: "center",
                     alignItems: "center",
                     backgroundColor: "rgba(0, 0, 0, 0.5)",
-                  }}>
+                  }}
+                >
                   <View
                     style={{
                       backgroundColor: "white",
                       padding: 20,
                       borderRadius: 8,
-                    }}>
+                    }}
+                  >
                     <Text
                       style={{
                         fontSize: 18,
                         fontWeight: "bold",
                         marginBottom: 10,
-                      }}>
+                      }}
+                    >
                       Transaction created successfully
                     </Text>
                     <Button
@@ -136,6 +146,6 @@ const CreateTransactionScreen = () => {
       </View>
       <StatusBar style="auto" />
     </ImageBackground>
-  )
-}
-export default CreateTransactionScreen
+  );
+};
+export default CreateTransactionScreen;
